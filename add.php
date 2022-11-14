1f0a0b8550cb10c8957b7105b9224752a38ae755
AlekGDEV <AlexandreGDEV@outlook.com>
2022-11-14 09:34:18 -0300

    atualizando o cadastro para retirar os erros

--- add.php
+++ add.php
@@ -1,3 +1,14 @@
+<?php
+    include('./config.php');
+    if(isset($_POST['submeter'])){
+        $nome = $_POST['valor_nome'];
+        $matricula = $_POST['valor_matricula'];
+        $cidade = $_POST['valor_cidade'];
+        $post = "INSERT INTO `ativ1`.`alunos` (`nome`, `matricula`, `cidade`) VALUES ('{$nome}','{$matricula}','{$cidade}');";
+        mysqli_query($connection, $post);   
+    }
+?>
+
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 <body style='background-color: #666'>
     <div class="container mt-5">
@@ -16,19 +27,12 @@
                 <label for="cidade" class="text-light mt-3">Cidade: </label>
                 <input name="valor_cidade" id="cidade" type="text" class="form-control bg-light text-start w-75" placeholder="Exemplo: Fortaleza" required>
     
-                <input type="submit" class="btn btn-outline-success text-light mt-3 w-75" placeholder="Enviar">
+                <input name="submeter" type="submit" class="btn btn-outline-success text-light mt-3 w-75" placeholder="Enviar">
             </div>
         </form>
         <div class="d-flex justify-content-center">
             <a href="/" class="btn btn-outline-light mt-2 w-50">Voltar ao inicio</a>
         </div>
     </div>
-    <?php error_reporting(0);
-        include('./config.php');
-            $nome = $_POST['valor_nome'];
-            $matricula = $_POST['valor_matricula'];
-            $cidade = $_POST['valor_cidade'];
-            $post = "INSERT INTO `ativ1`.`alunos` (`nome`, `matricula`, `cidade`) VALUES ('{$nome}','{$matricula}','{$cidade}');";
-            mysqli_query($connection, $post);
-    ?>
+    
 </body>
